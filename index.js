3250d2c8e0310aaadf33589603ea80641e7376f2
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-01-15 09:12:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"]; 
+
+function writeCards(nameArr, eventName) {
+   let array = []
+   for (let i in nameArr) {
+      array.push(`Thank you, ${nameArr[i]}, for the wonderful ${eventName} gift!`)
+   }
+   return array
+} 
+
+function countDown(num) {
+   while (num >= 0) {
+      console.log(num)
+      num--
+   }
+}
+
+debugger
